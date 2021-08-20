02ed4de2ce6aa95a83b3de1aca154183e2647b25
WardPearce <wardpearce@protonmail.com>
2021-08-20 18:56:12 +1200

    Added confirm email tests

--- aioaccount/tests/__init__.py
+++ aioaccount/tests/__init__.py
@@ -1,9 +1,12 @@
 from .test_create_account import TestCreateAccount, TestCreateAccountSqlSmtp
 from .test_to_user import TestToUser, TestToUserSqlSmtp
+from .test_confirm_email import TestEmailConfirm, TestEmailConfirmMongo
 
 __all__ = [
     "TestCreateAccount",
     "TestCreateAccountSqlSmtp",
     "TestToUser",
-    "TestToUserSqlSmtp"
+    "TestToUserSqlSmtp",
+    "TestEmailConfirm",
+    "TestEmailConfirmMongo"
 ]
