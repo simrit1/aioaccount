a35b622767cd65e472101755e393ed97aa2a3ba1
WardPearce <wardpearce@protonmail.com>
2021-08-23 10:34:02 +1200

    Ooops wrong project name

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'dathost'
+project = 'aioaccount'
 copyright = '2021, WardPearce'
 author = 'WardPearce'
 
